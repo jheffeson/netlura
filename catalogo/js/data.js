3ea7e927dc6129b8d6ea9f38129d92b2f3a2f944
Jheffeson Pereira Leal <jheffeson.leal@gmail.com>
2026-03-27 18:53:49 -0300

    fix: corrigir erro no carregamento assíncrono do catálogo

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,41 +1,116 @@
 export const categories = [
-    {
-        title: "Épicos",
-        items: [
-            {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
-            },
-            {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
-            },
-            {
-                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
-            },
-            {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
-            },
-        ]
-    },
-    {
-        title: "Séries",
-        items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-        ]
-    },
-    {
-        title: "Para maratonar",
-        items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-        ]
-    }
+  {
+    title: "Épicos",
+    items: [
+      {
+        // Breaking Bad
+        title: "Breaking Bad",
+        img: "https://musicnonstop.uol.com.br/wp-content/uploads/2025/09/Breaking-Bad.jpg",
+        top10: true,
+        badge: "Série",
+        badgeColor: "blue",
+        progress: 0,
+        youtube: "https://www.youtube.com/watch?v=2gTC4uWP3_Y",
+      },
+      {
+        // Stranger Things
+        title: "Stranger Things",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7nv-9_hny0jAVh5pA60MZjvxVk8eBtCqWYJiqzar5xGI-zpnbDtVa0XxlIYFpHzvlw8NG5KjgKwdZxSmrJA_8brd_w3TMtdlqjFFA14c&s=10",
+        progress: 0,
+        youtube: "https://www.youtube.com/watch?v=U4sndXXtrtE",
+      },
+      {
+        // Round 6
+        title: "Round 6",
+        img: "https://oespecialista.safra.com.br/wp-content/uploads/2021/10/squid-game-round-6-netflix.jpg",
+        progress: 0,
+        youtube: "https://www.youtube.com/watch?v=oqxAJKy0ii4",
+      },
+      {
+        // Peaky Blinders
+        title: "Peaky Blinders",
+        img: "https://m.media-amazon.com/images/S/pv-target-images/5c3e0d6b9ad0460aa7e63bb1e1d56c4b75722a98f467f5aa8d22650c27a71e1b.jpg",
+        progress: 0,
+        youtube: "https://www.youtube.com/watch?v=Ruyl8_PT_y8",
+      },
+    ],
+  },
+  {
+    title: "Séries",
+    items: [
+      {
+        // The Witcher
+        title: "The Witcher",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrvN6FhTJrrgPqxgUzK_vcE_tCDAJqE7tKGA&s",
+        top10: true,
+        badge: "Nova temporada",
+        badgeColor: "red",
+        youtube: "https://www.youtube.com/watch?v=ndl1W4ltcmg",
+      },
+      {
+        // The Crown
+        title: "The Crown",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ5MwrQd4dVXroQBpEFlhbsW8hOkYx85I2h2N2MagRZ_agUg6Iu4m-GQ24VtFMktlTY9uzCFXPgAGdMr-XK9DVphKDiBV7Qqb3is4ypCDw&s=10",
+        top10: true,
+        youtube: "https://www.youtube.com/watch?v=JWtnJjn6ng0",
+      },
+      {
+        // Dark
+        title: "Dark",
+        img: "https://br.web.img2.acsta.net/pictures/19/06/11/21/33/0075880.jpg",
+        badge: "Novo episódio",
+        badgeColor: "red",
+        youtube: "https://www.youtube.com/watch?v=rrwycJ08PSA",
+      },
+      {
+        // La Casa de Papel
+        title: "La Casa de Papel",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRtgPeOC1E2VV2UNBrqkNtTs1gXMTNChc63A&s=10",
+        badge: "Novidade",
+        badgeColor: "red",
+        youtube: "https://www.youtube.com/watch?v=iS5xXr-GOnM",
+      },
+    ],
+  },
+  {
+    title: "Para maratonar",
+    items: [
+      {
+        // Doctor House
+        title: "House M.D.",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVYtVnleC770L9H3fzUnKSYV9HDxHQI8oDAQ&s",
+        top10: true,
+        badge: "Série",
+        badgeColor: "blue",
+        youtube: "https://www.youtube.com/watch?v=MczMB8nU1sY",
+      },
+      {
+        // You
+        title: "You",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgjjcbI4iRMYVyeVPuZ2ioeA7aaGZLhulv_u-eErHaIP0cUbd721VFvDIYR5tXQGyaf7c6nMyfRpNU6q4Sv52GHF33h5Te2_EQIQw9rQcODA&s=10",
+        top10: true,
+        badge: "Nova temporada",
+        badgeColor: "red",
+        youtube: "https://www.youtube.com/watch?v=mNkyrdzzWok",
+      },
+      {
+        // House of Cards
+        title: "House of Cards",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAaaQdd7SFdDesq-gVlvwMgQ_bgrrvSXlaVp-SgLjjjZTfgIWS9y_8DL24NhEEdW8aQ2p-6t2goQ1D3ASt7Srj_fmpel8BPHN5fS2zcgZk&s=10",
+        top10: true,
+        badge: "Série",
+        badgeColor: "darkred", // Um vermelho mais escuro combina com o tom político da série
+        youtube: "https://www.youtube.com/watch?v=mpVD25j-LCo",
+      },
+      {
+        // Teen Wolf
+        title: "Teen Wolf",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW4u55-W0pTheETb-T94_uDvPpV5J9LtI2cEYPHKrhNgC2BvF0AkwVKvH2pRUUXcQJ_CQfXriSKwJs8k77UsYpbdel2ynVoyznzxmiDOv0zA&s=10",
+        top10: true,
+        badge: "Série",
+        badgeColor: "silver",
+        youtube: "https://www.youtube.com/watch?v=ES3mhnBgAGM",
+      },
+    ],
+  },
 ];
